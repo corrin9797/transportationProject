6ab99a8bd06442e8e15cd21c55ed73cf1f1ee14d
Corrin <corrinoff@gmail.com>
2017-10-21 13:47:45 -0400

    project part 1

---
+++ Transportation project/Assets/scripts/cubeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnMouseDown()
+    {
+        if (gameController.redCubeExists)
+        {
+            gameController.redCube.GetComponent<Renderer>().material.color = Color.white;
+        }
+        gameController.redCube = this.gameObject;
+        gameController.redCubeExists = true;
+        gameController.redCube.GetComponent<Renderer>().material.color = Color.red;
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Transportation project/Assets/scripts/gameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+    //Used for making cubes
+    Vector3 cubePosition;
+    public GameObject cubePrefab;
+    GameObject currentCube;
+
+    //Used for tracking which cube user has clicked if any
+    public static GameObject redCube;
+    public static bool redCubeExists = false;
+
+    // Use this for initialization
+    void MakeCube(int xPos,int yPos)
+    {
+        cubePosition = new Vector3(xPos, yPos, 0);
+
+        //                                    Quaternion, as of right now, is a magic word.
+        currentCube = Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+        currentCube.GetComponent<Renderer>().material.color = Color.white;
+        currentCube.AddComponent<cubeScript>();
+    }
+    
+    void Start () {
+        for (int i=-8; i<8; i++)
+        {
+            MakeCube(i, 0);
+            //This is somewhat inefficient, but also much easier to work with.
+            //I could just have the last cube be called the red cube to avoid null references,
+            //but I know we're going to rewrite this later and this way makes rewriting easier.
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}

b9cd2535fc7f998e71fcf8ed30a7ef22b0815de6
Corrin <corrinoff@gmail.com>
2017-10-25 15:24:45 -0400

    Added arrow key movement and array to track X,Y

--- Transportation project/Assets/scripts/gameController.cs
+++ Transportation project/Assets/scripts/gameController.cs
@@ -7,11 +7,20 @@ public class gameController : MonoBehaviour {
     Vector3 cubePosition;
     public GameObject cubePrefab;
     GameObject currentCube;
+    int airplaneX;
+    int airplaneY;
 
     //Used for tracking which cube user has clicked if any, and whether the airplane is active
     public static GameObject redCube;
     public static bool redCubeExists = false;
     public static bool airplaneIsActive = true;
+
+    public static GameObject[,] cubeArray = new GameObject[16,9];
+    //Array of arrays that tracks cubes
+    //[x,y] x==0 at left and y==0 at bottom
+
+
+    
 
     // Use this for initialization
     GameObject MakeCube(int xPos,int yPos)
@@ -28,25 +37,26 @@ public class gameController : MonoBehaviour {
     void Start () {
         for (int i=-8; i<8; i++)
         {
-            for (int j = 4; j > -5; j--){
+            for (int j = -4; j < 5; j++){
                 currentCube = MakeCube(i, j);
-                if (!redCubeExists)
-                {
-                    currentCube.GetComponent<Renderer>().material.color = Color.red;
-                    redCubeExists = true;
-                    redCube = currentCube;
-                }
+                
+                cubeArray[i + 8,j+4] = currentCube;
             }
             
             
         }
-        
+        cubeArray[0,8].GetComponent<Renderer>().material.color = Color.red;
+        redCubeExists = true;
+        redCube = cubeArray[0, 8];
+        airplaneX = 0;
+        airplaneY = 8;
+
     }
     public static void clickUpdate(GameObject clickedCube)
     {
         if (airplaneIsActive)
         {
-            if (redCubeExists) //We don't need this anymore because the game starts with a red cube
+            if (redCubeExists)                //We don't need this anymore because the game starts with a red cube
                                               //But I'm keeping it in case it doesn't later
                                               //Come to think of it, does C sharp have try/catch?
                                               //Or, as the TA's suggested, check if redCube is null.
@@ -58,23 +68,40 @@ public class gameController : MonoBehaviour {
                     airplaneIsActive = false;
                     //There's no way to reactivate the airplane, but a way wasn't specified in the outline.
                 }
-                else
-                {
-                    redCube.GetComponent<Renderer>().material.color = Color.white;
-                }
-            }
-            if (redCube != clickedCube)
-            {
-                redCube = clickedCube;
-                redCubeExists = true;
-               redCube.GetComponent<Renderer>().material.color = Color.red;
             }
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown("down") && airplaneY > 0)
+        {
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
+            redCube = cubeArray[airplaneX, airplaneY-1];
+            airplaneY--;
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown("up") && airplaneY < 8)
+        {
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
+            redCube = cubeArray[airplaneX, airplaneY + 1];
+            airplaneY++;
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown("left") && airplaneX > 0)
+        {
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
+            redCube = cubeArray[airplaneX-1, airplaneY];
+            airplaneX--;
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown("right") && airplaneX < 15)
+        {
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
+            redCube = cubeArray[airplaneX+1, airplaneY];
+            airplaneX++;
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
     
 }

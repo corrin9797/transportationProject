dd32724c1c1589a993526cb2d4a02a06ee5ae120
Corrin <corrinoff@gmail.com>
2017-10-30 15:19:07 -0400

    parts 3 and 4 added

--- Transportation project/Assets/scripts/gameController.cs
+++ Transportation project/Assets/scripts/gameController.cs
@@ -14,13 +14,24 @@ public class gameController : MonoBehaviour {
     public static GameObject redCube;
     public static bool redCubeExists = false;
     public static bool airplaneIsActive = true;
+    
 
-    public static GameObject[,] cubeArray = new GameObject[16,9];
     //Array of arrays that tracks cubes
     //[x,y] x==0 at left and y==0 at bottom
+    public static GameObject[,] cubeArray = new GameObject[16, 9];
+
+    //These variables track turn timers.
+    public float turnTime = 1.5f;
+    float timeToNextTurn = 1.5f;
+    //Yes, I should have used an enum. But I couldn't figure out how to get them to work, and the internet didn't tell me how.
+    string selectedMovement = "";
+
+    //Score and cargo variable
+    string onScreenScore="";
+    public int airplaneCargo = 0;
+    int playerScore = 0;
 
 
-    
 
     // Use this for initialization
     GameObject MakeCube(int xPos,int yPos)
@@ -71,37 +82,111 @@ public class gameController : MonoBehaviour {
             }
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("down") && airplaneY > 0)
-        {
-            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
-            redCube = cubeArray[airplaneX, airplaneY-1];
-            airplaneY--;
-            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
-        }
-        if (Input.GetKeyDown("up") && airplaneY < 8)
+	//Directional movement. Assumes you aren't letting it run off the grid.
+    void moveUp()
+    {
+        if (airplaneY < 8)
         {
             cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
             redCube = cubeArray[airplaneX, airplaneY + 1];
             airplaneY++;
             cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
         }
-        if (Input.GetKeyDown("left") && airplaneX > 0)
+    }
+    void moveLeft()
+    {
+        if (airplaneX > 0)
         {
             cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
-            redCube = cubeArray[airplaneX-1, airplaneY];
+            redCube = cubeArray[airplaneX - 1, airplaneY];
             airplaneX--;
             cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
         }
-        if (Input.GetKeyDown("right") && airplaneX < 15)
+    }
+    void moveRight()
+    {
+        if (airplaneX < 15)
         {
             cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
-            redCube = cubeArray[airplaneX+1, airplaneY];
+            redCube = cubeArray[airplaneX + 1, airplaneY];
             airplaneX++;
             cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
         }
     }
-    
+    void moveDown()
+    {
+        if (airplaneY > 0)
+        {
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
+            redCube = cubeArray[airplaneX, airplaneY - 1];
+            airplaneY--;
+            cubeArray[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //Rules did not specify if cargo was earned at start of turn, end of turn, or only if you haven't moved.
+        //I made it so as the airplane only collects cargo if it hasn't moved.
+        if (airplaneX==0 && airplaneY == 8 && timeToNextTurn < Time.time && selectedMovement=="")
+        {
+            airplaneCargo += 10;
+            if (airplaneCargo > 90)
+            {
+                airplaneCargo = 90;
+            }
+        }
+        if (airplaneX == 15 && airplaneY == 0 && timeToNextTurn < Time.time && selectedMovement == "")
+        {
+            playerScore += airplaneCargo;
+            airplaneCargo = 0;
+        }
+            if (timeToNextTurn < Time.time)
+        {
+            print("Moo");
+            timeToNextTurn += turnTime;
+            if (selectedMovement=="up")
+            {
+                moveUp();
+            }
+            if (selectedMovement=="down")
+            {
+                moveDown();
+            }
+            if (selectedMovement=="right")
+            {
+                moveRight();
+            }
+            if (selectedMovement=="left")
+            {
+                moveLeft();
+            }
+            selectedMovement = "";
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            selectedMovement="down";
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            selectedMovement="up";
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            selectedMovement="left";
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            selectedMovement="right";
+        }
+        cubeArray[15, 0].GetComponent<Renderer>().material.color = Color.black;
+        
+    }
+    //NOTE TO SELF: it's OnGUI with a CAPITAL O. That's why it's not showing up and not throwing an error.
+    void OnGUI()
+    {
+        onScreenScore ="Score: " + playerScore;
+
+        GUI.Box(new Rect(4,4,100,20), onScreenScore);
+    }
 }
